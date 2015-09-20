9c441b2344addf6b9d94d7d17f2679354edb24e8
Henrique Silva <hensansi@users.noreply.github.com>
2015-09-20 16:53:37 -0400

    Make angular app dynamic

--- angular2/entrypoint.sh
+++ angular2/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-nohup tsc -m commonjs -t es5 --emitDecoratorMetadata --experimentalDecorators app.ts 0<&- &>/dev/null &
+nohup tsc -m commonjs -watch -t es5 --emitDecoratorMetadata --experimentalDecorators app.ts 0<&- &>/dev/null &
 exec "$@"
