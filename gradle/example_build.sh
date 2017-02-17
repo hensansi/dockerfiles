2eea84caebd399892de4ade98e9abc2570ce5619
Henrique Silva <hensansi@gmail.com>
2017-02-17 19:50:18 +0100

    Update gradle build script example

--- gradle/example_build.sh
+++ gradle/example_build.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-
-set -ue
-
-DOCKER=${DOCKER_CMD:-$(command -v docker)}
-APP_DIR="/app"
+set -e
 USER="-u=$UID:`id -g $UID`"
-
-${DOCKER} run -it ${USER} -v $(pwd):${APP_DIR} gradle
+docker run -it ${USER} -v ${PWD}:/app gradle
